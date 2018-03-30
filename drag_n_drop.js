db068eefd53ae3caf063490b4c32b36e06b5ef28
Marc Goodman <marc.goodman@pcc.edu>
2018-03-30 11:01:01 -0700

    Move JavaScript and CSS into separate shared files.

---
+++ drag_n_drop.js
@@ -0,0 +1,61 @@
+$(
+  function() {
+    var rows = [];
+    var tiles = [];
+    for (var i = 0; i < 5; i++) {
+      rows[i] = $('<div></div>');
+    }
+    letters.forEach(function (l, index) {
+      if (l.letter == 'skip') {
+        var tile = null;
+        var target = $('<div class="skip">&nbsp;</div>');
+      } else {
+        var tile = $('<div class="tile" data-char="' + l.letter + '"><span class="letter">' + l.code + '</span></div>');
+        var target = $('<div class="target" data-char="' + l.letter + '"><span class="letter">' + l.letter + '</span></div>');
+      }
+      rows[index % 5].append(target);
+      if (tile) {
+        tile.data('order', Math.random());
+        tiles.push(tile);
+      }
+    });
+    rows.forEach(function (r) { $('#targets').append(r); });
+    for (var i = 0; i < 5; i++) {
+      rows[i] = $('<div></div>');
+      $('#tiles').append(rows[i]);
+    }
+    tiles.sort(function(a, b) {
+      var v1 = a.data('order'), v2 = b.data('order');
+      if (v1 < v2) return -1;
+      if (v1 > v2) return 1;
+      return 0;
+    }).forEach(function (tile, index) {
+      rows[index >> 4].append(tile);
+    });
+    $('.tile').draggable({
+      revert: function(target) {
+        if (!target || this.data('char') != target.data('char')) return true;
+        var spos = this.position();
+        var tpos = target.position();
+        this.position({
+          my: "top left",
+          at: "top left",
+          of: target
+        }, 'slow');
+        return false;
+      },
+      revertDuration: 100,
+      start: function() {
+        $(this).css('z-index', 20);
+      },
+      stop: function() {
+        $(this).css('z-index', 10);
+      }
+    });
+    $('.target').droppable({
+      classes: {
+        "ui-droppable-hover": "droppable_hover"
+      }
+    });
+  }
+)
